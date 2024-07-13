f84ef247fe2333a1a559b0a65ca6c0c3bbf1bd24
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-07-13 12:16:24 +0530

    13/07/2024

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use('/api/v1/user', userRouter)
 app.use('/api/v1/job', jobRouter)
 app.use('/api/v1/application', applicationRouter)
 
-dbConnection();
+dbConnection(); //database ke connection ke liyee call kiya!
 
 
 
