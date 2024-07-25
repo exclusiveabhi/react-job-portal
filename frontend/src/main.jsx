0d40de44e261184f3c3bfd4b36468bbe73f0b857
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-07-25 10:50:32 +0530

    25/07/2024

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Authorized nhi hai toh different page pr redirect kr do!
+export const Context = createContext({ isAuthorized: false }); //isAuthorized initally false!
+
+const AppWrapper = () => {
+  const [isAuthorized, setisAuthorized] = useState(false);
+  const [user, setuser] = useState({});
+  return (
+    <Context.Provider value={{ isAuthorized, setisAuthorized, user, setuser }}>
+      <App></App>
+    </Context.Provider>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppWrapper></AppWrapper>
+  </React.StrictMode>
+);
