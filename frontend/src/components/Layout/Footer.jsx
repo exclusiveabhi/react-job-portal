39b0882c2b862995448a898b41fdcf7d0b585385
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-07-26 10:45:28 +0530

    26/07/2024

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import {Context} from "../../main"
+import {Link} from "react-router-dom"
+import { FaGithub , FaLinkedin} from "react-icons/fa"
+import { SiLeetcode } from "react-icons/si";
+import { RiInstagramFill} from "react-icons/ri"
 function Footer() {
+  const {isAuthorized}  = useContext(Context)
   return (
-    <div>Footer</div>
+    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
+<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>
+  <Link to={'/'} target='_blank'><FaGithub></FaGithub></Link>
+  <Link to={'/'} target='_blank'><SiLeetcode></SiLeetcode></Link>
+  <Link to={'/'} target='_blank'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'/'} target='_blank'><RiInstagramFill></RiInstagramFill></Link>
+</div>
+      
+    </footer>
   )
 }
 
