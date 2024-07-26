bad026761c28b1c8541088d3ef5f1f96385f5add
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-07-26 12:48:14 +0530

    26/07/2024

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -1,107 +1,134 @@
 import React, { useContext, useState } from "react";
-import {Context} from "../../main"
-import toast from "react-hot-toast";
-import { Link, Navigate } from "react-router-dom";
-import { FaPencilAlt, FaRegUser  } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
 import { MdOutlineMailOutline } from "react-icons/md";
-import { FaPhoneFlip } from "react-icons/fa6";
-import axios from "axios";
 import { RiLock2Fill } from "react-icons/ri";
+import { FaPencilAlt } from "react-icons/fa";
+import { FaPhoneFlip } from "react-icons/fa6";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context } from "../../main";
 
-function Register() {
+const Register = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
   const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
-  const { isAuthorized, setisAuthorized, user, setuser } = useContext(Context);
 
-  //user register
+  const { isAuthorized, setisAuthorized, user, setUser } = useContext(Context);
+
   const handleRegister = async (e) => {
-    e.preventDefault(); //default refreshing to stop kr diya hai!
+    e.preventDefault();
     try {
       const { data } = await axios.post(
         "http://localhost:8080/api/v1/user/register",
-        { name, email, password, phone, role },
+        { name, phone, email, role, password },
         {
-          withCredentials: true,
           headers: {
             "Content-Type": "application/json",
           },
+          withCredentials: true,
         }
       );
-      toast.sucess(data.message);
+      toast.success(data.message);
       setName("");
       setEmail("");
       setPassword("");
       setPhone("");
       setRole("");
-      setisAuthorized(true);
+      setIsAuthorized(true);
     } catch (error) {
       toast.error(error.response.data.message);
     }
   };
 
   if(isAuthorized){
-return <Navigate to={"/"}></Navigate>
+    return <Navigate to={'/'}/>
   }
-  return <>
- <div className="authPage">
-  <div className="container">
-    <div className="header">
-      <img src="/JobZeelogo.png" alt="logo" />
-      <h3>Create a new account</h3>
-    </div>
-    <form>
-      <div className="inputTag">
-        <label>Register As</label>
-        <div>
-          <select value = {role} onChange={(e)=> setRole(e.target.value)}>
-            <option value = "">Select Role</option>
-            <option value = "Job Seeker">Job Seeker</option>
-            <option value = "Employer">Employer</option>
-          </select>
-          <FaRegUser></FaRegUser>
+
+
+  return (
+    <>
+      <section className="authPage">
+        <div className="container">
+          <div className="header">
+            <img src="/JobZeelogo.png" alt="logo" />
+            <h3>Create a new account</h3>
+          </div>
+          <form>
+            <div className="inputTag">
+              <label>Register As</label>
+              <div>
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="">Select Role</option>
+                  <option value="Job Seeker">Job Seeker</option>
+                  <option value="Employer">Employer</option>
+                  
+                </select>
+                <FaRegUser />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Name</label>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <FaPencilAlt />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Email Address</label>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Enter your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MdOutlineMailOutline />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Phone Number</label>
+              <div>
+                <input
+                  type="number"
+                  placeholder="Enter your phone number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+                <FaPhoneFlip />
+              </div>
+            </div>
+            <div className="inputTag">
+              <label>Password</label>
+              <div>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <RiLock2Fill />
+              </div>
+            </div>
+            <button type="submit" onClick={handleRegister}>
+              Register
+            </button>
+            <Link to={"/login"}>Login Now</Link>
+          </form>
         </div>
-      </div>
-      <div className="inputTag">
-        <label>Name</label>
-        <div>
-         <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter your name"/>
-         <FaPencilAlt></FaPencilAlt>
+        <div className="banner">
+          <img src="/register.png" alt="login" />
         </div>
-      </div>
-      <div className="inputTag">
-        <label>Email Address</label>
-        <div>
-         <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter your email address"/>
-         <MdOutlineMailOutline></MdOutlineMailOutline>
-        </div>
-      </div>
-      <div className="inputTag">
-        <label>Phone Number</label>
-        <div>
-         <input type="number" value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder="Enter your phone number"/>
-         <FaPhoneFlip/>
-        </div>
-      </div>
-      <div className="inputTag">
-        <label>Password</label>
-        <div>
-         <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter your password"/>
-         <RiLock2Fill/>
-        </div>
-      </div>
-      <button onClick={handleRegister} type="submit">Register</button>
-      <Link to={'/login'}>Login Now</Link>
-    </form>
-  </div>
-  <div className="banner">
-    <img src="/register.png" alt="register" />
-  </div>
- </div>
-  
-  </>;
-}
+      </section>
+    </>
+  );
+};
 
 export default Register;
