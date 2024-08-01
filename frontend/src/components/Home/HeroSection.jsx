de4f99a1077235c83d4cdbddbcc39497039e16ae
exclusiveabhi <imexclusiveabhi@gmail.com>
2024-08-01 11:24:24 +0530

    01/08/2024

--- frontend/src/components/Home/HeroSection.jsx
+++ frontend/src/components/Home/HeroSection.jsx
@@ -1,6 +1,33 @@
 import React from "react";
+import { FaBuilding, FaSuitcase, FaUsers, FaUserPlus } from "react-icons/fa";
 
 function HeroSection() {
+  const details = [
+    {
+      id: 1,
+      title: "1,23,441",
+      subTitle: "Live Job",
+      icon: <FaSuitcase />,
+    },
+    {
+      id: 2,
+      title: "91220",
+      subTitle: "Companies",
+      icon: <FaBuilding />,
+    },
+    {
+      id: 3,
+      title: "2,34,200",
+      subTitle: "Job Seekers",
+      icon: <FaUsers />,
+    },
+    {
+      id: 4,
+      title: "1,03,761",
+      subTitle: "Employers",
+      icon: <FaUserPlus />,
+    },
+  ];
   return (
     <div className="heroSection">
       <div className="container">
@@ -15,10 +42,23 @@ function HeroSection() {
           </p>
         </div>
         <div className="image">
-            <img src="/heroS.jpg" alt="hero" />
+          <img src="/heroS.jpg" alt="hero" />
         </div>
       </div>
-      
+      <div className="details">
+        {details.map((element) => {
+          //details pr loop chalega .map se or ek ek item ki value card mai jake paint hogi!
+          return (
+            <div className="card" key={element.id}>
+              <div className="icon">{element.icon}</div>
+              <div className="content">
+                <p>{element.title}</p>
+                <p>{element.subTitle}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- frontend/src/components/Home/HowItWorks.jsx
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
 
 function HowItWorks() {
   return (
-    <div>HowItWorks</div>
-  )
+    <div className="howitworks">
+      <div className="container">
+        <h3>How JobZee Works</h3>
+        <div className="banner">
+          <div className="card">
+            <FaUserPlus></FaUserPlus>
+            <p>Create a account</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem corporis perspiciatis iure.
+            </p>
+          </div>
+          <div className="card">
+            <MdFindInPage/>
+            <p>Find a Job/Post a Job</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem corporis perspiciatis iure.
+            </p>
+          </div>
+          <div className="card">
+            <IoMdSend></IoMdSend>
+            <p>Apply For Job/Recruit Suitable Candidates</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem corporis perspiciatis iure.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HowItWorks
+export default HowItWorks;
